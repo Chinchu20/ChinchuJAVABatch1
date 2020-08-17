b53ca0be126246be6212b1f862d85c2cf1e236b3
Chinchu20 <69795611+Chinchu20@users.noreply.github.com>
2020-08-17 17:45:16 +0530

    Add files via upload
    DAY-1 Challenge (Nest4Code)

---
+++ Day1/RestaurantBill.java
@@ -0,0 +1,19 @@
+package com.srcodes.day1;
+
+public class RestaurantBill {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+						float P1=70;
+				float P2=20;
+				float P3=10;
+				float tax_percent=18;
+				float total_price=P1+P2+P3;
+				float Amount=(tax_percent/100)*total_price;
+				System.out.print("Final Amount : "+Amount);
+			}
+
+		}
+		 
+
+	
